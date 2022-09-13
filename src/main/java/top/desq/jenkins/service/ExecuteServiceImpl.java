461a306f3aa01a7622fb2b4106aecb2a5cc15f38
Oleksandr Vashchenko <oleksandr.vashchenko@sap.com>
2022-09-13 11:05:40 -0500

    add logger

--- src/main/java/top/desq/jenkins/service/ExecuteServiceImpl.java
+++ src/main/java/top/desq/jenkins/service/ExecuteServiceImpl.java
@@ -1,9 +1,13 @@
 package top.desq.jenkins.service;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class ExecuteServiceImpl implements ExecuteService {
 
     @Override
     public void run() {
+        log.info("Running execute service...");
         System.out.println("Running execute service...");
     }
 }
